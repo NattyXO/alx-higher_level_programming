a5ca31cd2a906db35e40edb9a2344ecd65b2a663
NattyXO <nattyxo2018@gmail.com>
2022-12-20 04:12:30 -0800

    0

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const first = fs.readFileSync(process.argv[2], 'utf8');
+const second = fs.readFileSync(process.argv[3], 'utf8');
+const data = first + second;
+fs.appendFileSync(process.argv[4], data);
